4123be6f7249becba737808bf37d802a595d57a6
Marina Kostenko <kostenkomarin@gmail.com>
2022-05-30 17:29:22 -0700

    Initial Commit

---
+++ TimezoneFollower/TimezoneFollowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimezoneFollowerApp.swift
+//  TimezoneFollower
+//
+//  Created by Marina Kostenko on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimezoneFollowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimezoneFollowerUITests/TimezoneFollowerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TimezoneFollowerUITestsLaunchTests.swift
+//  TimezoneFollowerUITests
+//
+//  Created by Marina Kostenko on 30.05.2022.
+//
+
+import XCTest
+
+class TimezoneFollowerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
