da0efffa353f533e2b9cac78885700920e9b042b
Marina Kostenko <kostenkomarin@gmail.com>
2022-09-15 18:26:41 -0700

    swagger geneartion updated

--- timezonefollower-rest/src/main/java/com/codemari/timezonefollowerrest/config/SwaggerConfig.java
+++ timezonefollower-rest/src/main/java/com/codemari/timezonefollowerrest/config/SwaggerConfig.java
@@ -2,11 +2,19 @@ package com.codemari.timezonefollowerrest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +24,13 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Timezone Follower - REST api")
+                .description("Timezone follower application.").termsOfServiceUrl("")
+                .version("0.0.1")
                 .build();
     }
 
---
+++ timezonefollower-rest/src/main/java/com/codemari/timezonefollowerrest/config/WebSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.codemari.timezonefollowerrest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+    };
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().
+                antMatchers(AUTH_WHITELIST).permitAll().antMatchers("/**").authenticated();
+    }
+}
