be503d42d26dfd5f895be5233827d771f1fc50e0
Marina Kostenko <kostenkomarin@gmail.com>
2022-09-15 19:10:29 -0700

    deprecated class updated

--- timezonefollower-rest/src/main/java/com/codemari/timezonefollowerrest/config/SecurityConfiguration.java
+++ timezonefollower-rest/src/main/java/com/codemari/timezonefollowerrest/config/SecurityConfiguration.java
@@ -1,15 +1,17 @@
 package com.codemari.timezonefollowerrest.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
     private static final String[] AUTH_WHITELIST = {
             // -- Swagger UI v2
             "/v2/api-docs",
@@ -19,12 +21,17 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             "/configuration/security",
             "/swagger-ui.html",
             "/webjars/**",
+            "/swagger-ui/**"
     };
 
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().
-                antMatchers(AUTH_WHITELIST).permitAll().antMatchers("/**").authenticated();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST)
+                .permitAll().antMatchers("/**")
+                .authenticated();
+        return http.build();
     }
 }
